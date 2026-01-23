b6ebd07c2c511b51804536d55e53553095df8471
Paul <137211766+PaulWeber-co@users.noreply.github.com>
2026-01-23 08:10:12 +0000

    Setup

---
+++ ship_game/view.py
@@ -0,0 +1,26 @@
+#grid ausgeben
+import numpy as np
+"""
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+fig, ax = plt.subplots()
+
+
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+
+ax.set_xticks(range(0, 11))
+ax.set_yticks(range(0, 11))
+
+ax.set_yticklabels([chr(ord('A') + i) for i in range(11)])
+ax.grid(True)
+
+rect = patches.Rectangle((3, 5), 1, 3, linewidth=1,
+                         edgecolor='black', facecolor='orange')
+ax.add_patch(rect)
+
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
+"""
