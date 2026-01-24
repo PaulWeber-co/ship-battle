6522410bc34f09cf6167fd068f1e412ee97ea37d
Paul <PaulWeber@protonmail.ch>
2026-01-24 11:57:55 +0100

    test g14

--- ship_game/model.py
+++ ship_game/model.py
@@ -1,10 +1,2 @@
-from controller import RPSController
-from view import RPSView
-from model import RPSModel
-"""
-if __name__ == "__main__":
-    model = Model()
-    view = View()
-    controller = Controller(model, view)
-    controller.run()
-"""
+class RPSModel:
+    pass
